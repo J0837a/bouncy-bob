8169ea904548484cde9295212249d05e9d1f4022
marin <jh0837a@student.american.edu>
2022-09-21 15:41:40 -0700

    Work

--- src/main.py
+++ src/main.py
@@ -6,25 +6,35 @@ Width, Height = 900, 500
 Window = game.display.set_mode((Width, Height))
 game.display.set_caption("Bouncy Bob") #Add a cool lile title thing later
 FPS = 60
-
-
-Player = player(loadFromAssets("spaceship_red.png"), 50, 50, 450, 450)
+Backround = game.transform.scale(loadFromAssets("space.png"), (Width, Height))
+bulletHit = game.USEREVENT + 1
+# game.event.post(game.event.Event(bulletHit)) to do this event ig
+Player = player(loadFromAssets("spaceship_red.png"), 20, 20, 450, 450)
 Player.rotate(180)
 
 objects = [Player]
 
 def createInvaders(amount):
-    spacing = 900 / amount
+    spacing = (900 / amount)
 
     for x in range(amount):
-        Invader = imageObjectRect(loadFromAssets("spaceship_yellow.png"), 50, 50, (x * spacing) + 100, 100)
+        Invader = invader(loadFromAssets("spaceship_yellow.png"), 20, 20, (x * spacing), 100)
         objects.append(Invader)
 
 def draw_window():
-    Window.fill((0, 255, 0))
+    Window.blit(Backround, (0, 0))
     for i in objects:
         i.draw(Window)
+        if i.collisions:
+            i.collisionCheck(objects)
+        if i.randomEvents:
+            i.randomEvent(objects)
     game.display.update()
+
+def outOfBoundsCheck(objects):
+    for i in objects:
+        if (i.x < 0 or i.y < 0) or (i.x > Width or i.y > Height):
+            objects.remove(i)
 
 
 def handleMovement():
@@ -39,22 +49,24 @@ def handleMovement():
             return
         Player.x += 4
     if pressed[pygame.K_w]:
-        if Player.shots > 3:
+        if Player.shotCooldown > 0:
             return
-        objects.append(laser(Player))
-        Player.shots += 1
+        objects.append(laser(Player, True))
+        Player.shotCooldown = 20
 
 def master():
     clock = game.time.Clock()
     play = True
-    createInvaders(5)
+    createInvaders(10)
     while play:
         clock.tick(FPS)
+        Player.shotCooldown -= 1
         for event in game.event.get():
             if event.type == game.QUIT:
                 play = False
 
         handleMovement()
+        outOfBoundsCheck(objects)
         draw_window()
 
     game.quit()
