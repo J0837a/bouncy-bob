0c7cf573f05262fcd9edbfec8fc6c660a9c70768
marin <jh0837a@student.american.edu>
2022-09-23 00:07:46 -0700

    Mhm

--- src/basics.py
+++ src/basics.py
@@ -1,7 +1,55 @@
-import pygame as game
+import pygame
 import os
 
-bulletHitInvader = game.USEREVENT + 1
+passCheckpoint = pygame.USEREVENT + 1
+hitWall = pygame.USEREVENT + 2
 
 def loadFromAssets(file):
-    return game.image.load(os.path.join("Assets", file))
+    return pygame.image.load(os.path.join("Assets", file))
+
+class wall:
+    def __init__(self, y, top):
+        self.x = 800
+        self.y = y
+
+        if top:
+            self.rect = pygame.Rect(800, 0, 30, 0 + y)
+        else:
+            self.rect = pygame.Rect(800, y, 30, 500 - y)
+
+    def tick(self, window):
+        pygame.draw.rect(window, (0, 0, 255), self)
+        self.rect.x -= 2
+
+class scoreMarker:
+    def __init__(self):
+        self.x = 800
+        self.rect = pygame.Rect(800 + 15, 0, 15, 500)
+
+    def tick(self, window):
+        pygame.draw.rect(window, (255, 0, 0), self)
+        self.rect.x -= 2
+
+class player:
+    def __init__(self):
+        self.x = 300
+        self.y = 250
+        self.rect = pygame.Rect(self.x, self.y, 30, 30)
+        self.jumping = False
+        self.jumpTicks = 0
+        self.cooldownTicks = -1
+
+    def tick(self, window):
+        pygame.draw.rect(window, (0, 255, 0), self)
+
+        if self.jumping and self.jumpTicks < 0:
+            self.jumping = False
+
+        if not self.jumping:
+            self.rect.y += 3
+        else:
+            self.rect.y -= 3
+            self.jumpTicks -= 1
+
+        self.cooldownTicks -= 1
+
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from basics import *
 import pygame as game
+import random as rand
 
 game.font.init()
 game.mixer.init()
@@ -9,20 +10,43 @@ Window = game.display.set_mode((Width, Height))
 game.display.set_caption("Bouncy Bob") #Add a cool lile title thing later
 FPS = 60
 
-def draw_window():
+objects = [player()]
+
+def spawnPipes():
+    center = rand.randint(30, 470)
+    objects.append(wall(center - 40, True))
+    objects.append(wall(center + 40, False))
+    objects.append(scoreMarker())
+def tick():
+    Window.fill((255, 255, 255))
+    for obj in objects:
+        obj.tick(Window)
+
     game.display.update()
 
+def checkPlayerHits():
+    pass
 
 def master():
     clock = game.time.Clock()
     play = True
+    spawnPipes()
     while play:
         clock.tick(FPS)
         for event in game.event.get():
             if event.type == game.QUIT:
                 play = False
 
-        draw_window()
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_SPACE]:
+            if objects[0].cooldownTicks < 0:
+                objects[0].jumping = True
+                objects[0].jumpTicks = 10
+                objects[0].cooldownTicks = 10
+
+        checkPlayerHits()
+        tick()
 
     game.quit()
 
